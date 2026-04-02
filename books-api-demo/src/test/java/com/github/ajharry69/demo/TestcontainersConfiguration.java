13ae0173b31fb23db833481a887d125b3ae2b144
Orinda Harrison (Mitch) <mitchke@duck.com>
2026-04-03 00:19:50 +0300

    Fix indentation across source and test files for consistency and readability

--- books-api-demo/src/test/java/com/github/ajharry69/demo/TestcontainersConfiguration.java
+++ books-api-demo/src/test/java/com/github/ajharry69/demo/TestcontainersConfiguration.java
@@ -9,10 +9,10 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
 
-	@Bean
-	@ServiceConnection
-	OracleContainer oracleFreeContainer() {
-		return new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"));
-	}
+    @Bean
+    @ServiceConnection
+    OracleContainer oracleFreeContainer() {
+        return new OracleContainer(DockerImageName.parse("gvenzl/oracle-free:latest"));
+    }
 
 }
