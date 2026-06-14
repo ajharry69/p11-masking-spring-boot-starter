e1f456d563407ccb804f39e8d96d70b68cbfb0f1
Orinda Harrison (Mitch) <mitchke@duck.com>
2026-06-14 11:08:25 +0300

    Add comprehensive documentation to `MaskingStyle` enum to describe masking styles

--- p11-masking-spring-boot-starter/src/main/java/co/ke/xently/log/mask/MaskingStyle.java
+++ p11-masking-spring-boot-starter/src/main/java/co/ke/xently/log/mask/MaskingStyle.java
@@ -1,3 +1,24 @@
 package co.ke.xently.log.mask;
 
-public enum MaskingStyle {DEFAULT, FULL, PARTIAL, LAST4}
+public enum MaskingStyle {
+    /**
+     * Represents the default masking style. This is the primary or fallback style used when no specific
+     * masking style is provided or explicitly required.
+     * The behaviour associated with the DEFAULT style depends on its usage context.
+     */
+    DEFAULT,
+    /**
+     * Represents the full masking style. This style is used to mask all characters in a string.
+     */
+    FULL,
+    /**
+     * Represents the partial masking style. This style is used to mask all characters in a string except
+     * the first (@LogProperties.P11.Masking.partialMaskingExemption) characters.
+     */
+    PARTIAL,
+    /**
+     * Represents the last 4 masking style. This style is used to mask all the characters in a string
+     * except the last 4 characters.
+     */
+    LAST4
+}
